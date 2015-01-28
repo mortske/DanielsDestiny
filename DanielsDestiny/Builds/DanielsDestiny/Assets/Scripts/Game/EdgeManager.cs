2c61cb134de9644798a202ac798d7eb03c3b9939
mortske <mattias.ekstrom@hotmail.com>
2015-01-28 11:00:04 +0100

    Added a gamescene for the actual level

--- DanielsDestiny/Builds/DanielsDestiny/Assets/Scripts/Game/EdgeManager.cs
+++ DanielsDestiny/Builds/DanielsDestiny/Assets/Scripts/Game/EdgeManager.cs
@@ -14,6 +14,7 @@ public class EdgeManager : MonoBehaviour
     {
         if (!started)
         {
+            Debug.Log("started death timer");
             started = true;
             StartCoroutine(StartCountdown());
             playerStatus = player.GetComponent<Status>();
