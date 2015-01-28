e35cb02ce292767cf2a57a0c42b7710a6ca22102
Robert_c_C <robert.c.carlsson@gmail.com>
2015-01-28 14:07:53 +0100

    Reworked Save/Load of resources to write and read from xml document

---
+++ DanielsDestiny/Builds/DanielsDestiny/Assets/Biome.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Biome : MonoBehaviour {
+	List<Resource> _resources = new List<Resource>();
+
+	void Start()
+	{
+		foreach(Resource r in transform.GetComponentsInChildren<Resource>())
+		{
+			_resources.Add(r);
+		}
+	}
+	public int GetLength()
+	{
+		return _resources.Count;
+	}
+
+	public void LoadResources(string r)
+	{
+		for(int i = 0; i < _resources.Count; i++)
+		{
+			int firstDigit = (int)(r[i]) - 48;
+			_resources[i].Load(firstDigit == 1);
+		}
+	}
+	public string SaveResources()
+	{
+		string test = "";
+		for(int i = 0; i < _resources.Count; i++)
+		{
+			test = (test + _resources[i].Save());
+		}
+		return test;
+	}
+}
---
+++ DanielsDestiny/Builds/DanielsDestiny/Assets/BiomeManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+public class BiomeManager : MonoBehaviour {
+	public List<Biome> pieces = new List<Biome>();
+	public BiomeSave save;
+
+	string path = "Assets/Files/Save.xml";
+	// Use this for initialization
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.Period))
+		{
+			LoadBiomes();
+		}
+		if(Input.GetKeyUp(KeyCode.Comma))
+		{
+			SaveBiomeResource();
+		}
+	}
+	void LoadBiomes()
+	{
+		if (File.Exists(path))
+		{
+			StreamReader sr = new StreamReader(path);
+			XmlSerializer x = new System.Xml.Serialization.XmlSerializer(save.GetType());
+			save = x.Deserialize(sr) as BiomeSave;
+			sr.Close();
+		}
+		else
+		{
+			Directory.CreateDirectory("Assets/Files/");
+			File.Create(path);
+		}
+		for(int i = 0; i < pieces.Count; i++)
+		{
+			pieces[i].LoadResources(save.saveString[i]);
+		}
+	}
+	void SaveBiomes()
+	{
+		StreamWriter sw = new StreamWriter(path);
+		XmlSerializer x = new System.Xml.Serialization.XmlSerializer(save.GetType());
+		x.Serialize(sw, save);
+		sw.Close();
+	}
+	public void SaveBiomeResource()
+	{
+		for(int i = 0; i < pieces.Count; i++)
+		{
+			if(save.saveString.Count < pieces.Count)
+			{
+				save.saveString.Add(pieces[i].SaveResources());
+			}
+			else
+			{
+				save.saveString[i] = (pieces[i].SaveResources());
+			}
+		}
+		SaveBiomes();
+	}
+}
+[System.Serializable]
+public class BiomeSave
+{
+	public List<string> saveString = new List<string>();
+}
