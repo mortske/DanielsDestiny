933dc3ab487a7fe26c4b1c1aadd29377181129ff
mortske <mattias.ekstrom@hotmail.com>
2015-02-03 10:35:48 +0100

    Added a Watersource

---
+++ DanielsDestiny/Builds/DanielsDestiny/Assets/Scripts/Game/Watersource.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Watersource : MonoBehaviour 
+{
+    public float adjustment;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Player.instance.status.thirst.adjustCur(adjustment);
+            }
+        }
+    }
+}
