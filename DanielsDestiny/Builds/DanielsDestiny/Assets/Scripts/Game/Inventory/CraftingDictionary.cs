48b852f3e08deb86d18908a77de32cc2fe66891d
mortske <mattias.ekstrom@hotmail.com>
2015-02-04 12:45:55 +0100

    made dictionary for crafting + a custom editor that the designers will never look at

---
+++ DanielsDestiny/Builds/DanielsDestiny/Assets/Scripts/Game/Inventory/CraftingDictionary.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CraftingDictionary : MonoBehaviour 
+{
+	public List<Recepie> recepies;
+}
+
+[System.Serializable]
+public class Recepie
+{
+    public string recepieName = "";
+    public List<GameObject> items;
+    public List<int> amount;
+    public GameObject result;
+}
