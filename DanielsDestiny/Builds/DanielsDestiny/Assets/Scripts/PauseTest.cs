429a5aa7162ef2109e919f96430f91c18b280b6e
Joltrix <Dimitris.th@hotmail.com>
2015-02-09 13:34:52 +0100

    Added Menu

---
+++ DanielsDestiny/Builds/DanielsDestiny/Assets/Scripts/PauseTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseTest : MonoBehaviour {
+
+	public static bool isPaused;
+	public static bool returnToMenu;
+	public static bool isInPauseMenu;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape)) {
+			if(!isPaused) {
+				//pause
+				PauseSystem.Pause(true);
+				isPaused = true;
+			} else if (MenuScript.canBePaused == false && isInPauseMenu == true) {
+				MenuScript.canBePaused = true;
+				isPaused = false;
+				PauseSystem.Pause(false);
+			} else {
+				//unpause
+				PauseSystem.Pause(false);
+				isPaused = false;
+			}
+		}
+	}
+}
