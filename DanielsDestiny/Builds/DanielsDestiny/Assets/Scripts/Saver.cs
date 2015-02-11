0cda65775718915ff736e37df88d04dd01ea035b
Robert <robert.c.carlsson@gmail.com>
2015-02-11 14:11:40 +0100

    SaveLoadBed Added

---
+++ DanielsDestiny/Builds/DanielsDestiny/Assets/Scripts/Saver.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Saver : MonoBehaviour {
+	bool _active = false;
+
+	// Update is called once per frame
+	void Update () {
+		if(_active)
+		{
+			if(Input.GetKeyUp(KeyCode.E))
+			{
+				Save();
+			}
+		}
+	}
+	void OnTriggerStay(Collider other)
+	{
+		if(other.tag == "Player")
+			_active = true;
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Player")
+			_active = false;
+	}
+	void Save()
+	{
+		GameTime.instance.SaveTime();
+		BiomeManager.instance.SaveGame();
+	}
+}
