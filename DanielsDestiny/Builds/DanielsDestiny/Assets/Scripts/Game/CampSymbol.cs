d791290441de51c9997daec92adc6d2ee6734e92
Toxo8 <daniel.leven@hotmail.com>
2015-02-17 15:52:24 +0100

    camp symbol added

---
+++ DanielsDestiny/Builds/DanielsDestiny/Assets/Scripts/Game/CampSymbol.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CampSymbol : MonoBehaviour {
+
+	Quaternion rot;
+	Transform target;
+	Vector3 startScale;
+	float startDistance;
+	float currDistance;
+	Camera campSymbolCam;
+
+	void Start()
+	{
+		campSymbolCam = Camera.allCameras[1];
+		target = Player.instance.transform;
+		startScale = transform.localScale;
+		startDistance = Vector3.Distance(transform.position, target.position);
+	}
+
+	void Update () 
+	{
+		rot = new Quaternion(0, Quaternion.LookRotation(target.position - transform.position).y, 0, Quaternion.LookRotation(target.position - transform.position).w);
+		transform.rotation = Quaternion.Slerp(transform.rotation, rot, 10 * Time.deltaTime);
+
+		currDistance = Vector3.Distance(transform.position, target.position) / 25;
+		transform.localScale = new Vector3(startScale.x * currDistance, startScale.y * currDistance, 0);
+
+		if(transform.localScale.x <= 2)
+		{
+			transform.localScale = startScale;
+			campSymbolCam.depth = -1;
+		}
+		else if(transform.localScale.x >= 80)
+		{
+			transform.localScale = new Vector3(80, 80, 0);
+		}
+		else
+		{
+			campSymbolCam.depth = 1;
+		}
+	}
+}
